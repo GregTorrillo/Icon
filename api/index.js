bfe9424a49251344ddf66d74c3eb610434a02a62
Greg Torrillo <greg.torrillo@gmail.com>
2022-10-09 16:53:21 -0400

    moved dotenv above stripe

--- api/index.js
+++ api/index.js
@@ -2,15 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+dotenv.config();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-const cors = require("cors");
-
-dotenv.config();
 const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 
 mongoose
   .connect(process.env.MONGO_URL)
